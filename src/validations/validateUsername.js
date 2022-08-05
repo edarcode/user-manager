625fed2262db461398f266f95981675c792069fe
edarcode <edarcode@gmail.com>
2022-08-05 08:57:12 -0500

    validando username

--- src/validations/validateUsername.js
+++ src/validations/validateUsername.js
@@ -1,4 +1,8 @@
+import { regex } from "../constants/regex";
+
 export const validateUsername = username => {
+	if (!regex.username.test(username)) return "Solo minúsculas y números";
+	if (regex.startNumber.test(username)) return "No puede empezar por números";
 	if (username.length < 6 || username.length > 15) {
 		return "Longitud entre 6 y 15";
 	}
